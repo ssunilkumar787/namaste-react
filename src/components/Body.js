f2f4c6c6610793b403aa6e2e3e81f87f3ce32197
Sunil <ssunilkumar787@gmail.com>
2024-12-10 11:29:02 +0530

    chap 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,29 @@
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
+// import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  if (listOfRestaurants?.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
